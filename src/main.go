aefa78d9584edd73617f8763a4eed15924908201
soomattsu <msoshi1202@gmail.com>
2026-08-02 01:15:29 +0900

    Implement all 4 endpoints

--- src/main.go
+++ src/main.go
@@ -5,28 +5,29 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"strconv"
 	"sync"
 )
 
 type Document struct {
-	Id     string `json:"id"`
+	Author string `json:"author"`
 	Title  string `json:"title"`
 	Status string `json:"status"`
 }
 
 type DocStorage struct {
-	documents map[string]Document // keyはidを想定
+	documents map[string]Document
 	mu        sync.RWMutex
 }
 
-var (
-	d1       = Document{"1", "First one yeahhhh", "v1"}
-	d2       = Document{"2", "Broken doc foo bar baz", "v2"}
-	d3       = Document{"3", "Can we do something? Yes we can!", "v2"}
-	dList    = []Document{d1, d2, d3}
-	dMap     = map[string]Document{"1": d1, "2": d2, "3": d3}
-	dStorage = DocStorage{documents: dMap}
-)
+var dStorage = DocStorage{
+	documents: map[string]Document{
+		"1": {"Tanaka", "First one yeahhhh", "public"},
+		"2": {"Sato", "Broken doc foo bar baz", "private"},
+		"3": {"Nagano", "Can we do something? Yes we can!", "public"},
+	},
+	mu: sync.RWMutex{},
+}
 
 func healthzHandler() func(w http.ResponseWriter, _ *http.Request) {
 	return func(w http.ResponseWriter, _ *http.Request) {
@@ -34,11 +35,17 @@ func healthzHandler() func(w http.ResponseWriter, _ *http.Request) {
 	}
 }
 
+// グローバルなデータストアからDocument一覧を取得してJSONエンコードし、bodyとして返す
 func getDocumentsHandler() func(w http.ResponseWriter, _ *http.Request) {
 	return func(w http.ResponseWriter, _ *http.Request) {
-		documentsJson, err := json.Marshal(dList)
-		// documentsJson, err = json.Marshal(make(chan int)) <- 500 errorが返るケース
-		if err != nil {
+		dStorage.mu.RLock()
+		defer dStorage.mu.RUnlock()
+		ds := make([]Document, 0, len(dStorage.documents))
+		for _, d := range dStorage.documents {
+			ds = append(ds, d)
+		}
+		documentsJson, err := json.Marshal(ds)
+		if err != nil { // デコード失敗時は500を返す （ex. json.Marshal(make(chan int))）
 			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 			return
 		}
@@ -47,19 +54,62 @@ func getDocumentsHandler() func(w http.ResponseWriter, _ *http.Request) {
 	}
 }
 
-func main() {
-	// handler関数の初期化
-	helloHandler := func(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Hello, World from net/http!\n")
+// グローバルなデータストアから指定されたidのDocumentを取得してJSONエンコードし、bodyとして返す
+func getDocumentByIdHandler() func(w http.ResponseWriter, req *http.Request) {
+	return func(w http.ResponseWriter, req *http.Request) {
+		docId := req.PathValue("id")
+		dStorage.mu.RLock()
+		defer dStorage.mu.RUnlock()
+		doc, ok := dStorage.documents[docId]
+		if !ok {
+			http.Error(w, http.StatusText(http.StatusNotFound), http.StatusNotFound)
+			return
+		}
+		docJson, err := json.Marshal(doc)
+		if err != nil {
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		w.Write(docJson)
 	}
+}
 
+// req bodyのJSON文字列をデコードし、新規Documentとしてグローバルなデータストアに追加する
+func postDocumentHandler() func(w http.ResponseWriter, req *http.Request) {
+	return func(w http.ResponseWriter, req *http.Request) {
+		// req body取得
+		body, err := io.ReadAll(req.Body)
+		if err != nil {
+			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+			return
+		}
+		// JSON文字列をデコード
+		var doc Document
+		err = json.Unmarshal(body, &doc)
+		if err != nil {
+			http.Error(w, http.StatusText(http.StatusBadRequest), http.StatusBadRequest)
+			return
+		}
+		// 採番して格納
+		dStorage.mu.Lock()
+		defer dStorage.mu.Unlock()
+		docId := strconv.Itoa(len(dStorage.documents) + 1)
+		dStorage.documents[docId] = doc
+		w.Header().Set("Location", "/documents/"+docId)
+		w.WriteHeader(http.StatusCreated)
+	}
+}
+
+func main() {
 	// 任意のpathに対応するhandler関数を"DefaultServeMux"へ登録（ルーティング設定）
 	// - DefaultServeMux: net/httpパッケージにあらかじめ用意されている、グローバルなマルチプレクサ（ServeMux）のインスタンス
 	// - マルチプレクサ(Mux): N個の入力から、選択信号を元に、1個の出力を返す機構
 	//   - 複数の入力(pattern, handler)から、選択信号(req)を元に、1個の出力（res）を返す機構
-	http.HandleFunc("/", helloHandler)
 	http.HandleFunc("/healthz", healthzHandler())
 	http.HandleFunc("GET /documents", getDocumentsHandler())
+	http.HandleFunc("POST /documents", postDocumentHandler())
+	http.HandleFunc("GET /documents/{id}", getDocumentByIdHandler())
 
 	// http serverの起動。2nd argがnilなので、ルーティングにはDefaultServeMuxが使われる
 	log.Fatal(http.ListenAndServe(":8080", nil))
