97e6134194e7a62d616809f5834569a5b05c9d57
soomattsu <msoshi1202@gmail.com>
2026-08-07 00:15:22 +0900

    Implement Update and Delete for Post model

--- internal/handler/phase2.go
+++ internal/handler/phase2.go
@@ -17,6 +17,10 @@ type createPostInput struct {
 	Body   string `json:"body"`
 }
 
+type updatePostInput struct {
+	Body string `json:"body"`
+}
+
 type postsOutput struct {
 	ID        uint      `json:"id"`
 	UserID    uint      `json:"userId"`
@@ -105,7 +109,8 @@ func GetPostByID(w http.ResponseWriter, r *http.Request) {
 }
 
 func CreatePost(w http.ResponseWriter, r *http.Request) {
-	// クライアントが可能なfieldの限定と、ゼロ値や無効fieldを弾くvalidation
+	// 入力用structへの変換
+	// クライアントが指定可能なfieldの限定とvalidation（ゼロ値や無効fieldを弾く）
 	var in createPostInput
 	err := initDecoder(w, r).Decode(&in)
 	if err != nil || in.Body == "" || in.UserID == 0 {
@@ -131,9 +136,61 @@ func CreatePost(w http.ResponseWriter, r *http.Request) {
 }
 
 func UpdatePost(w http.ResponseWriter, r *http.Request) {
-	http.Error(w, http.StatusText(http.StatusNotImplemented), http.StatusNotImplemented)
+	// クエリ実行時、placeholderでwhere句を構成するなら、PathValue文字列をそのまま渡してもSQLインジェクションは起きない
+	// ここでは、MySQL側の暗黙変換による予期しない結果を予防するためにintへキャストする
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil {
+		log.Printf("Client error: specified invalid id: %v", err)
+		http.Error(w, "Client error: invalid Post ID", http.StatusBadRequest)
+		return
+	}
+	// 入力用structへの変換
+	// クライアントが指定可能なfieldの限定とvalidation（ゼロ値や無効fieldを弾く）
+	var in updatePostInput
+	err = initDecoder(w, r).Decode(&in)
+	if err != nil || in.Body == "" {
+		log.Printf("Client error: sent invalid body for PATCH: %+v (decode err: %v)", in, err)
+		http.Error(w, "Client error: only allowed to update body", http.StatusBadRequest)
+		return
+	}
+
+	// SELECT -> UPDATE
+	var post store.Post
+	if err := store.DB.First(&post, id).Error; err != nil {
+		// UPDATE対象が存在しなかったら即終了
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			log.Printf("Client error: Post to UPDATE not found with ID[%v]: %v", id, err)
+			http.Error(w, "Client error: no such Post to update", http.StatusNotFound)
+			return
+		}
+		log.Printf("Server error: failed to retrieve Post to UPDATE with ID[%v]: %v", id, err)
+		http.Error(w, "Server error: failed to find Post to update", http.StatusInternalServerError)
+		return
+	}
+	// SELECTに使ったFirst()は"Finisher Method"なので、戻り値の*gorm.DBにチェーンしてUpdates()すべきではない
+	// db.Updates()の引数にデータモデル以外のstructを渡すと、updated_atの自動更新が効かない！！
+	if err := store.DB.Model(&post).Updates(store.Post{Body: in.Body}).Error; err != nil {
+		log.Printf("Server error: failed to UPDATE Post with ID[%v]: %v", id, err)
+		http.Error(w, "Server error: failed to update Post", http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusNoContent)
 }
 
 func DeletePost(w http.ResponseWriter, r *http.Request) {
-	http.Error(w, http.StatusText(http.StatusNotImplemented), http.StatusNotImplemented)
+	// クエリ実行時、placeholderでwhere句を構成するなら、PathValue文字列をそのまま渡してもSQLインジェクションは起きない
+	// ここでは、MySQL側の暗黙変換による予期しない結果を予防するためにintへキャストする
+	id, err := strconv.Atoi(r.PathValue("id"))
+	if err != nil {
+		log.Printf("Client error: specified invalid id: %v", err)
+		http.Error(w, "Client error: invalid Post ID", http.StatusBadRequest)
+		return
+	}
+	// DELETEは、元々のレコード有無に関わらず冪等に204を返す
+	if err := store.DB.Delete(&store.Post{}, id).Error; err != nil {
+		log.Printf("Server error: failed to DELETE Post with ID[%v]: %v", id, err)
+		http.Error(w, "Server error: failed to delete Post", http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusNoContent)
 }
